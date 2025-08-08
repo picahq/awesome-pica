9de289ca082ba4b94702b3f376dc1ced14d23457
Satadru Bhowmik <110734942+sagacious-satadru@users.noreply.github.com>
2025-08-08 15:59:30 +0530

    Updated the index.ts to add the super agent

--- mastra-pica-multipurpose-ai-agent/src/mastra/index.ts
+++ mastra-pica-multipurpose-ai-agent/src/mastra/index.ts
@@ -4,9 +4,10 @@ import { PinoLogger } from '@mastra/loggers';
 import { LibSQLStore } from '@mastra/libsql';
 import { projectAssistantAgent } from './agents/project-assistant-agent';
 import { codeReviewerAgent } from './agents/code-reviewer-agent';
+import { superAgent } from './agents/super-agent';
 
 export const mastra = new Mastra({
-  agents: { projectAssistantAgent, codeReviewerAgent },
+  agents: { projectAssistantAgent, codeReviewerAgent, superAgent },
   storage: new LibSQLStore({
     // Use persistent file storage for telemetry, evals, and agent memory
     url: "file:./mastra.db",
